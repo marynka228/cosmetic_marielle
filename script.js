7195055128648d1fce402a6f981f178d38dfc9f6
вашmarynka228 <marino4ka.gladka@gmail.com>
2024-11-20 22:19:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let slides = document.querySelector('.slides');
+let slide = document.querySelectorAll('.slide');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(i) {
+  index += i;
+  if (index < 0) {
+    index = slide.length - 1;
+  } else if (index >= slide.length) {
+    index = 0;
+  }
+  slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+prev.addEventListener('click', () => showSlide(-1));
+next.addEventListener('click', () => showSlide(1));
